22d2cfb5258fe997a72936a2a69e20daf8898a2d
TJ Alaimo <tjalaimo@gmail.com>
2024-08-22 10:01:26 -0700

    fix navbar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,5 +1,5 @@
 import React, {useState} from 'react';
-import { AppBar, Toolbar, Typography, Popover, IconButton, useMediaQuery, Box, Grid } from '@mui/material';
+import { AppBar, Toolbar, Typography, SwipeableDrawer, IconButton, useMediaQuery, Box, Grid, Divider, List, ListItem, ListItemButton, ListItemIcon, ListItemText } from '@mui/material';
 import HomeIcon from '@mui/icons-material/Home';
 import MedicalServicesIcon from '@mui/icons-material/MedicalServices';
 import FeedIcon from '@mui/icons-material/Feed';
@@ -7,6 +7,12 @@ import GroupIcon from '@mui/icons-material/Group';
 import Avatar from '@mui/material/Avatar';
 import MenuIcon from '@mui/icons-material/Menu';
 import LocalHospitalIcon from '@mui/icons-material/LocalHospital';
+import CalendarMonthIcon from '@mui/icons-material/CalendarMonth';
+import MedicalInformationIcon from '@mui/icons-material/MedicalInformation';
+import ChatBubbleOutlineIcon from '@mui/icons-material/ChatBubbleOutline';
+import SettingsIcon from '@mui/icons-material/Settings';
+import HelpOutlineIcon from '@mui/icons-material/HelpOutline';
+import LogoutIcon from '@mui/icons-material/Logout';
 import { useNavigate } from 'react-router-dom';
 
 const NavBar = () => {
@@ -21,6 +27,18 @@ const NavBar = () => {
     { label: 'Networks', icon: <GroupIcon />, path: '/networks' },
     { label: 'Providers', icon: <LocalHospitalIcon />, path: '/providers' }
   ];
+
+  const secondaryNavLinks = [
+    { label: 'Messages', icon: <ChatBubbleOutlineIcon />, path: '/' }, 
+    { label: 'MyHealth', icon: <MedicalInformationIcon  />, path: '/' },
+    { label: 'Shared Calendar', icon: <CalendarMonthIcon />, path: '/feed' },
+  ]
+ 
+  const teritaryNavLinks = [
+    { label: 'Settings', icon: <SettingsIcon />, path: '/' }, 
+    { label: 'Help', icon: <HelpOutlineIcon  />, path: '/' },
+    { label: 'Logout', icon: <LogoutIcon />, path: '/feed' },
+  ]
 
   return (
     
@@ -78,27 +96,57 @@ const NavBar = () => {
                 </Box>
               </Grid>
             </Grid>
-            <Popover     
+            <SwipeableDrawer
+              anchor={'right'}
               open={openMenu}
               onClose={ () => {setOpenMenu(false)}}
             >
-            {navLinks.map((link) => (
-              <>
-                  <IconButton
-                  key={link.label}
-                  color="inherit"
-                  onClick={() => {setOpenMenu(false); navigate(link.path)}}
-                  aria-label={link.label}                    
-                  >
-                  {link.icon}
-                  </IconButton>
-                  <Typography>{link.label}</Typography>
-                </>
-              ))}
-            </Popover>
+              <Box sx={{ width: 250 }}>
+                <Box sx={{ p: 2 }}>
+                  <Avatar alt="Remy Sharp" src="/static/images/avatar/2.jpg" />
+                </Box>
+                <List>
+                  {navLinks.map((link) => (
+                    <ListItem key={link.label} disablePadding>
+                      <ListItemButton onClick={() => { navigate(link.path); setOpenMenu(false); }}>
+                        <ListItemIcon>
+                          {link.icon}
+                        </ListItemIcon>
+                        <ListItemText primary={link.label} />
+                      </ListItemButton>
+                    </ListItem>
+                  ))}
+                </List>
+                <Divider variant="middle" />
+                <List>
+                  {secondaryNavLinks.map((link) => (
+                    <ListItem key={link.label} disablePadding>
+                      <ListItemButton onClick={() => { navigate(link.path); setOpenMenu(false); }}>
+                        <ListItemIcon>
+                          {link.icon}
+                        </ListItemIcon>
+                        <ListItemText primary={link.label} />
+                      </ListItemButton>
+                    </ListItem>
+                  ))}
+                </List>
+                <Divider variant='middle' />
+                <List>
+                  {teritaryNavLinks.map((link) => (
+                    <ListItem key={link.label} disablePadding>
+                      <ListItemButton onClick={() => { navigate(link.path); setOpenMenu(false); }}>
+                        <ListItemIcon>
+                          {link.icon}
+                        </ListItemIcon>
+                        <ListItemText primary={link.label} />
+                      </ListItemButton>
+                    </ListItem>
+                  ))}
+                </List>
+              </Box>
+            </SwipeableDrawer>
           </>
-        )}
-
+        )}        
       </Toolbar>
       
 
